18b63e5aebf066eca37d2c963cbe3f8f07904035
cartycrabber <cartycrabber@gmail.com>
2013-11-21 10:14:28 -0500

    creation and writing text file logic added

--- FBLA Project/Form1.cs
+++ FBLA Project/Form1.cs
@@ -7,14 +7,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace FBLA_Project
 {
     public partial class Form1 : Form
     {
+
+        #region Variables
+
+        string dataFolder = Application.StartupPath + @"\Data";
+        string participantsFile = @"\PARTICIPANTS.txt";
+
+        #endregion
+
         public Form1()
         {
             InitializeComponent();
+            if (!Directory.Exists(dataFolder))
+            {
+                Directory.CreateDirectory(dataFolder);
+            }
+            if (!File.Exists(dataFolder + participantsFile))
+            {
+                File.Create(dataFolder + participantsFile).Close();
+            }
+//            Directory.CreateDirectory(dataFolder);
+//            File.Create(dataFolder + participantsFile);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            writeParticipant();
+        }
+
+        private void writeParticipant()
+        {
+            using (System.IO.StreamWriter participants = new System.IO.StreamWriter(Application.StartupPath + @"\Data\PARTICIPANTS.txt", true))
+            {
+                participants.WriteLine("This is a test");
+            }
         }
     }
 }
