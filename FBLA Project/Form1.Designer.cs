85380dc80880c88591a0b472150fad80bfca0aad
cartycrabber <cartycrabber@gmail.com>
2014-01-31 04:16:12 -0500

    Formatting of the main form

--- FBLA Project/Form1.Designer.cs
+++ FBLA Project/Form1.Designer.cs
@@ -46,9 +46,10 @@
             // 
             // enterButton
             // 
-            this.enterButton.Location = new System.Drawing.Point(400, 416);
+            this.enterButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enterButton.Location = new System.Drawing.Point(408, 396);
             this.enterButton.Name = "enterButton";
-            this.enterButton.Size = new System.Drawing.Size(110, 34);
+            this.enterButton.Size = new System.Drawing.Size(168, 54);
             this.enterButton.TabIndex = 0;
             this.enterButton.Text = "Enter Information";
             this.enterButton.UseVisualStyleBackColor = true;
@@ -111,35 +112,37 @@
             this.workshopComboBox.FormattingEnabled = true;
             this.workshopComboBox.Location = new System.Drawing.Point(12, 12);
             this.workshopComboBox.Name = "workshopComboBox";
-            this.workshopComboBox.Size = new System.Drawing.Size(417, 32);
+            this.workshopComboBox.Size = new System.Drawing.Size(476, 32);
             this.workshopComboBox.TabIndex = 6;
             this.workshopComboBox.Text = "Workshops";
             this.workshopComboBox.SelectedIndexChanged += new System.EventHandler(this.workshopComboBox_SelectedIndexChanged);
             // 
             // workshopTextBox
             // 
-            this.workshopTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.workshopTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.workshopTextBox.Location = new System.Drawing.Point(12, 50);
             this.workshopTextBox.Name = "workshopTextBox";
             this.workshopTextBox.ReadOnly = true;
-            this.workshopTextBox.Size = new System.Drawing.Size(620, 246);
+            this.workshopTextBox.Size = new System.Drawing.Size(714, 262);
             this.workshopTextBox.TabIndex = 8;
             this.workshopTextBox.Text = "";
             // 
             // chosenWorkBox
             // 
+            this.chosenWorkBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.chosenWorkBox.FormattingEnabled = true;
-            this.chosenWorkBox.Location = new System.Drawing.Point(638, 52);
+            this.chosenWorkBox.ItemHeight = 16;
+            this.chosenWorkBox.Location = new System.Drawing.Point(732, 52);
             this.chosenWorkBox.Name = "chosenWorkBox";
             this.chosenWorkBox.SelectionMode = System.Windows.Forms.SelectionMode.None;
-            this.chosenWorkBox.Size = new System.Drawing.Size(334, 277);
+            this.chosenWorkBox.Size = new System.Drawing.Size(240, 292);
             this.chosenWorkBox.TabIndex = 9;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(657, 13);
+            this.label1.Location = new System.Drawing.Point(696, 13);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(276, 31);
             this.label1.TabIndex = 10;
@@ -148,7 +151,7 @@
             // workButton
             // 
             this.workButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.workButton.Location = new System.Drawing.Point(436, 13);
+            this.workButton.Location = new System.Drawing.Point(494, 12);
             this.workButton.Name = "workButton";
             this.workButton.Size = new System.Drawing.Size(196, 30);
             this.workButton.TabIndex = 11;
@@ -160,7 +163,7 @@
             // 
             this.workshopDateTime.AutoSize = true;
             this.workshopDateTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.workshopDateTime.Location = new System.Drawing.Point(12, 299);
+            this.workshopDateTime.Location = new System.Drawing.Point(12, 315);
             this.workshopDateTime.Name = "workshopDateTime";
             this.workshopDateTime.Size = new System.Drawing.Size(183, 29);
             this.workshopDateTime.TabIndex = 12;
@@ -168,6 +171,7 @@
             // 
             // reportsButton
             // 
+            this.reportsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.reportsButton.Location = new System.Drawing.Point(897, 427);
             this.reportsButton.Name = "reportsButton";
             this.reportsButton.Size = new System.Drawing.Size(75, 23);
