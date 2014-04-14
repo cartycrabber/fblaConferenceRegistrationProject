aa46334e5feae24d0fe2aca3831b11c0ef8f8957
cartycrabber <cartycrabber@gmail.com>
2014-04-14 16:12:43 -0400

    Final Submission

---
+++ FBLA Project/ReportsForm.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace FBLA_Project
+{
+    partial class ReportsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReportsForm));
+            this.sortingMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.conferenceParticipantsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.workshopParticipantsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reportListBox = new System.Windows.Forms.ListBox();
+            this.sortTypeButton = new System.Windows.Forms.Button();
+            this.sortChapterButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.searchTextbox = new System.Windows.Forms.TextBox();
+            this.partSearchButton = new System.Windows.Forms.Button();
+            this.sortingMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sortingMenuStrip
+            // 
+            this.sortingMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.conferenceParticipantsToolStripMenuItem,
+            this.workshopParticipantsToolStripMenuItem});
+            this.sortingMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.sortingMenuStrip.Name = "sortingMenuStrip";
+            this.sortingMenuStrip.Size = new System.Drawing.Size(984, 24);
+            this.sortingMenuStrip.TabIndex = 0;
+            this.sortingMenuStrip.Text = "menuStrip1";
+            // 
+            // conferenceParticipantsToolStripMenuItem
+            // 
+            this.conferenceParticipantsToolStripMenuItem.Name = "conferenceParticipantsToolStripMenuItem";
+            this.conferenceParticipantsToolStripMenuItem.Size = new System.Drawing.Size(145, 20);
+            this.conferenceParticipantsToolStripMenuItem.Text = "Conference Participants";
+            // 
+            // workshopParticipantsToolStripMenuItem
+            // 
+            this.workshopParticipantsToolStripMenuItem.Name = "workshopParticipantsToolStripMenuItem";
+            this.workshopParticipantsToolStripMenuItem.Size = new System.Drawing.Size(138, 20);
+            this.workshopParticipantsToolStripMenuItem.Text = "Workshop Participants";
+            // 
+            // reportListBox
+            // 
+            this.reportListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.reportListBox.FormattingEnabled = true;
+            this.reportListBox.ItemHeight = 20;
+            this.reportListBox.Location = new System.Drawing.Point(12, 67);
+            this.reportListBox.Name = "reportListBox";
+            this.reportListBox.Size = new System.Drawing.Size(960, 364);
+            this.reportListBox.TabIndex = 1;
+            // 
+            // sortTypeButton
+            // 
+            this.sortTypeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sortTypeButton.Location = new System.Drawing.Point(114, 27);
+            this.sortTypeButton.Name = "sortTypeButton";
+            this.sortTypeButton.Size = new System.Drawing.Size(121, 34);
+            this.sortTypeButton.TabIndex = 2;
+            this.sortTypeButton.Text = "Participant Type";
+            this.sortTypeButton.UseVisualStyleBackColor = true;
+            this.sortTypeButton.Visible = false;
+            this.sortTypeButton.Click += new System.EventHandler(this.sortTypeButton_Click);
+            // 
+            // sortChapterButton
+            // 
+            this.sortChapterButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sortChapterButton.Location = new System.Drawing.Point(241, 27);
+            this.sortChapterButton.Name = "sortChapterButton";
+            this.sortChapterButton.Size = new System.Drawing.Size(129, 34);
+            this.sortChapterButton.TabIndex = 3;
+            this.sortChapterButton.Text = "Chapter Number";
+            this.sortChapterButton.UseVisualStyleBackColor = true;
+            this.sortChapterButton.Visible = false;
+            this.sortChapterButton.Click += new System.EventHandler(this.sortChapterButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Sort By:";
+            this.label1.Visible = false;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(915, 27);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(57, 35);
+            this.exitButton.TabIndex = 5;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // searchTextbox
+            // 
+            this.searchTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchTextbox.ForeColor = System.Drawing.Color.Gray;
+            this.searchTextbox.Location = new System.Drawing.Point(516, 30);
+            this.searchTextbox.Name = "searchTextbox";
+            this.searchTextbox.Size = new System.Drawing.Size(333, 31);
+            this.searchTextbox.TabIndex = 6;
+            this.searchTextbox.Text = "Search Participant Name";
+            this.searchTextbox.Enter += new System.EventHandler(this.searchTextbox_Enter);
+            this.searchTextbox.Leave += new System.EventHandler(this.searchTextbox_Leave);
+            // 
+            // partSearchButton
+            // 
+            this.partSearchButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.partSearchButton.Location = new System.Drawing.Point(432, 27);
+            this.partSearchButton.Name = "partSearchButton";
+            this.partSearchButton.Size = new System.Drawing.Size(78, 34);
+            this.partSearchButton.TabIndex = 7;
+            this.partSearchButton.Text = "Go";
+            this.partSearchButton.UseVisualStyleBackColor = true;
+            this.partSearchButton.Click += new System.EventHandler(this.partSearchButton_Click);
+            // 
+            // ReportsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 462);
+            this.Controls.Add(this.partSearchButton);
+            this.Controls.Add(this.searchTextbox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sortChapterButton);
+            this.Controls.Add(this.sortTypeButton);
+            this.Controls.Add(this.reportListBox);
+            this.Controls.Add(this.sortingMenuStrip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.sortingMenuStrip;
+            this.MaximizeBox = false;
+            this.Name = "ReportsForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Reports";
+            this.sortingMenuStrip.ResumeLayout(false);
+            this.sortingMenuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip sortingMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem conferenceParticipantsToolStripMenuItem;
+        private System.Windows.Forms.ListBox reportListBox;
+        private System.Windows.Forms.Button sortTypeButton;
+        private System.Windows.Forms.Button sortChapterButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem workshopParticipantsToolStripMenuItem;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.TextBox searchTextbox;
+        private System.Windows.Forms.Button partSearchButton;
+    }
+}
---
+++ FBLA Project/ReportsForm.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace FBLA_Project
+{
+    public partial class ReportsForm : Form
+    {
+        string dataFolder;
+        string participantsFile;
+        string workshopRegistrationFile;
+        List<string> reportList = new List<string>();
+        List<string> conferencesList;
+        List<string> typeList;
+        string[] workshopsRegArr;
+        string[] participantsArr;
+        Form mainForm;
+
+        public ReportsForm(Form form, List<string> conference, List<string> conferenceList, List<string> type, List<string> workshops, string data, string participants, string workshopRegistration)
+        {
+            InitializeComponent();
+            reportList.Clear();
+            reportListBox.Items.Clear();
+            label1.Visible = false;
+            sortChapterButton.Visible = false;
+            sortTypeButton.Visible = false;
+            mainForm = form;
+            dataFolder = MainForm.dataFolder;
+            participantsFile = MainForm.participantsFile;
+            workshopRegistrationFile = MainForm.workshopRegistrationsFile;
+            conferencesList = MainForm.conferenceInfoList;
+            typeList = MainForm.typeList;
+            Debug.WriteLine("Reports Form Intializing");
+            foreach(string conf in conferenceList)
+            {
+                Debug.WriteLine(conf);
+                ToolStripMenuItem x = new ToolStripMenuItem();
+                x.Text = conf;
+                x.Click += new EventHandler(confToolTipClick);
+                conferenceParticipantsToolStripMenuItem.DropDownItems.Add(x);
+            }
+            foreach(string work in workshops)
+            {
+                ToolStripMenuItem x = new ToolStripMenuItem();
+                x.Text = work;
+                x.Click += new EventHandler(workToolTipClick);
+                workshopParticipantsToolStripMenuItem.DropDownItems.Add(x);
+            }
+            workshopsRegArr = File.ReadAllLines(dataFolder + workshopRegistrationFile);
+            participantsArr = File.ReadAllLines(dataFolder + participantsFile);
+        }
+        void confToolTipClick(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            sortChapterButton.Visible = true;
+            sortTypeButton.Visible = true;
+            writeConferneceReport(sender);
+        }
+        void workToolTipClick(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            sortChapterButton.Visible = false;
+            sortTypeButton.Visible = false;
+            writeWorkshopReport(sender);
+        }
+
+        void writeConferneceReport(object conference)
+        {
+            reportListBox.Items.Clear();
+            reportList.Clear();
+            string conferenceID = conferencesList[conferencesList.FindIndex(x => x.Contains(conference.ToString())) - 1];
+            foreach(string participants in participantsArr)
+            {
+                string[] splitPart = participants.Split(',');
+                if(splitPart[1].Trim() == conferenceID.Trim())
+                {
+                    reportList.Add(splitPart[0] + ", " + splitPart[2] + ", " + splitPart[3] + ", " + splitPart[4] + ", " + splitPart[5]);
+                }
+            }
+            reportListBox.Items.AddRange(reportList.ToArray());
+        }
+
+        void writeWorkshopReport(object workshop)
+        {
+            reportListBox.Items.Clear();
+            reportList.Clear();
+            string workshopID = MainForm.workshopList[MainForm.workshopList.FindIndex(x => x.Contains(workshop.ToString()))][0].Trim();
+            foreach (string work in workshopsRegArr)
+            {
+                string[] splitWork = work.Split(',');
+                if (splitWork[1].Trim() == workshopID)
+                {
+                    foreach (string participants in participantsArr)
+                    {
+                        string[] splitPart = participants.Split(',');
+                        if(splitPart[0].Trim() == splitWork[0].Trim())
+                        {
+                            reportList.Add(splitPart[2] + ", " + splitPart[3] + ", " + splitPart[4] + ", " + splitPart[5]);
+                        }
+                    }
+                }
+            }
+            reportListBox.Items.AddRange(reportList.OrderBy(r => r.Split(',')[2]).ToArray());
+        }
+
+        private void sortTypeButton_Click(object sender, EventArgs e)
+        {
+            reportListBox.Items.Clear();
+            reportListBox.Items.AddRange(sortByType(reportList).ToArray());
+        }
+
+        private void sortChapterButton_Click(object sender, EventArgs e)
+        {
+            reportListBox.Items.Clear();
+            reportListBox.Items.AddRange(sortByChapter(reportList).ToArray());
+        }
+
+        private List<string> sortByType(List<string> list)
+        {
+            return list.OrderBy(r => r.Split(',')[1]).ThenBy(r2 => r2.Split(',')[3]).ToList();
+        }
+
+        private List<string> sortByChapter(List<string> list)
+        {
+            return list.OrderBy(r => r.Split(',')[4]).ThenBy(r => r.Split(',')[1]).ThenBy(r => r.Split(',')[3]).ToList();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            reportList.Clear();
+            reportListBox.Items.Clear();
+            label1.Visible = false;
+            sortChapterButton.Visible = false;
+            sortTypeButton.Visible = false;
+            Hide();
+            mainForm.Show();
+        }
+
+        private void searchTextbox_Enter(object sender, EventArgs e)
+        {
+            if (searchTextbox.Text == "Search Participant Name")
+            {
+                searchTextbox.Text = "";
+                searchTextbox.ForeColor = System.Drawing.Color.Black;
+            }
+        }
+
+        private void searchTextbox_Leave(object sender, EventArgs e)
+        {
+            if(searchTextbox.Text == "")
+            {
+                searchTextbox.Text = "Search Participant Name";
+                searchTextbox.ForeColor = System.Drawing.Color.Gray;
+            }
+        }
+
+        private void partSearchButton_Click(object sender, EventArgs e)
+        {
+            bool partFound = false;
+            string partID = null;
+            string partChap = null;
+            string partType = null;
+            string partConf = null;
+            string partName = null;
+            foreach(string participant in participantsArr)
+            {
+                string[] splitPart = participant.Split(',');
+                if (splitPart[3].TrimStart() + splitPart[4].TrimEnd() == searchTextbox.Text)
+                {
+                    partFound = true;
+                    partID = splitPart[0].Trim();
+                    partChap = splitPart[5].Trim();
+                    partType = splitPart[2].Trim();
+                    partConf = splitPart[1].Trim();
+                    partName = splitPart[3].TrimStart() + splitPart[4].TrimEnd();
+                    break;
+                }
+            }
+            if (partFound)
+            {
+                reportList.Clear();
+                reportListBox.Items.Clear();
+                reportList.Add(partName);
+                reportList.Add("Chapter: " + partChap);
+                reportList.Add("Participant Type: " + MainForm.participantTypes.First(x => x.Contains(partType)).Split(',')[1]);
+                reportList.Add("Conference: " + MainForm.conferences.First(x => x.Contains(partConf)).Split(',')[1]);
+                reportList.Add("");
+                reportList.Add("Registered Workshops: ");
+                foreach(string work in workshopsRegArr)
+                {
+                    if(work.Split(',')[0] == partID)
+                    {
+                        reportList.Add(MainForm.workshops.First(x => x.Contains(work.Split(',')[1])).Split(',')[2].Trim() + ", " + MainForm.workshops.First(x => x.Contains(work.Split(',')[1])).Split(',')[4].Trim() + ", " + MainForm.workshops.First(x => x.Contains(work.Split(',')[1])).Split(',')[5].Trim());
+                    }
+                }
+            }
+            else
+                MessageBox.Show("Participant Not Found. Did you spell it correctly?", "Error");
+            reportListBox.Items.AddRange(reportList.ToArray());
+        }
+    }
+}
